9be8fe914525d7d4aad340c205d12890b295e76d
LtReborn <lookket.111@gmail.com>
2019-03-17 16:06:42 +0700

    before do Module page Fin upload pic and weblink

--- app/src/main/java/com/example/wireless/Firstpage.java
+++ app/src/main/java/com/example/wireless/Firstpage.java
@@ -34,18 +34,5 @@ public class Firstpage extends AppCompatActivity{
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if(id == R.id.action) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
--- app/src/main/java/com/example/wireless/Module.java
+++ app/src/main/java/com/example/wireless/Module.java
@@ -1,5 +1,6 @@
 package com.example.wireless;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -7,6 +8,8 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 
@@ -33,6 +36,7 @@ public class Module extends AppCompatActivity {
         setContentView(R.layout.activity_module);
         Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
         mToolbar.setTitle(getString(R.string.app_name));
+        setSupportActionBar(mToolbar);
         mToolbar.setNavigationIcon(R.drawable.ic_arrow_back_black_24dp);
 
         mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
@@ -88,5 +92,27 @@ public class Module extends AppCompatActivity {
 
 
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.profile) {
+            Intent intent = new Intent(this,ProfilePage.class);
+            this.startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
