44217da2f4c2d47448a82a35b7972ad4aaecb3d4
LtReborn <lookket.111@gmail.com>
2019-04-10 13:27:09 +0700

    The rest= review + add comment

---
+++ app/src/main/java/com/example/wireless/Model/QuestionEx.java
@@ -0,0 +1,67 @@
+package com.example.wireless.Model;
+
+//objects are constructed for the use of exercise
+public class QuestionEx {
+    public String q, op1, op2, op3, op4, ans;
+
+    public QuestionEx(String q, String op1, String op2, String op3, String op4, String ans) {
+        this.q = q;
+        this.op1 = op1;
+        this.op2 = op2;
+        this.op3 = op3;
+        this.op4 = op4;
+        this.ans = ans;
+    }
+
+    public QuestionEx() {
+
+    }
+
+    public String getQ() {
+        return q;
+    }
+
+    public void setQ(String q) {
+        this.q = q;
+    }
+
+    public String getOp1() {
+        return op1;
+    }
+
+    public void setOp1(String op1) {
+        this.op1 = op1;
+    }
+
+    public String getOp2() {
+        return op2;
+    }
+
+    public void setOp2(String op2) {
+        this.op2 = op2;
+    }
+
+    public String getOp3() {
+        return op3;
+    }
+
+    public void setOp3(String op3) {
+        this.op3 = op3;
+    }
+
+    public String getOp4() {
+        return op4;
+    }
+
+    public void setOp4(String op4) {
+        this.op4 = op4;
+    }
+
+    public String getAns() {
+        return ans;
+    }
+
+    public void setAns(String ans) {
+        this.ans = ans;
+    }
+}
